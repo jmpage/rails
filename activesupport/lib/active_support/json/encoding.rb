83144feb70bdbbf7693a9956e02e40d18268effb
Godfrey Chan <godfreykfc@gmail.com>
2013-11-19 19:17:55 -0800

    Removed support for encoding BigDecimal as a JSON number
    This is because the new encoder will no longer support encode_json.
    Therefore our only choice is to return `to_i` or `to_s` in
    `BigDecimal#as_json`. Since casting a BigDecimal to an integer is
    most likely a lossy operation, we chose to encode it as a string.
    
    Support for encoding BigDecimal as a string will return via the
    `activesupport-json_encoder` gem.
    
    Conflicts:
    	activesupport/lib/active_support/core_ext/object/json.rb

--- activesupport/lib/active_support/json/encoding.rb
+++ activesupport/lib/active_support/json/encoding.rb
@@ -17,7 +17,6 @@ module ActiveSupport
   class << self
     delegate :use_standard_json_time_format, :use_standard_json_time_format=,
       :escape_html_entities_in_json, :escape_html_entities_in_json=,
-      :encode_big_decimal_as_string, :encode_big_decimal_as_string=,
       :to => :'ActiveSupport::JSON::Encoding'
   end
 
@@ -111,10 +110,6 @@ module ActiveSupport
         # to the Active Support legacy format.
         attr_accessor :use_standard_json_time_format
 
-        # If false, serializes BigDecimal objects as numeric instead of wrapping
-        # them in a string.
-        attr_accessor :encode_big_decimal_as_string
-
         attr_accessor :escape_regex
         attr_reader :escape_html_entities_in_json
 
@@ -138,7 +133,6 @@ module ActiveSupport
 
       self.use_standard_json_time_format = true
       self.escape_html_entities_in_json  = true
-      self.encode_big_decimal_as_string  = true
     end
   end
 end
